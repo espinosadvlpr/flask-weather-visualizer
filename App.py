4bfd1f05931d7d93d7060e265f3ad035dc2fa48e
David Espinosa <38819699+espinosadvlpr@users.noreply.github.com>
2023-04-23 22:40:35 -0500

    (fix): changing mysql data in server

--- App.py
+++ App.py
@@ -4,9 +4,9 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'admin1234'
-app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_USER'] = '[user]'
+app.config['MYSQL_PASSWORD'] = '[password]'
+app.config['MYSQL_DB'] = '[database_name]'
 mysql = MySQL(app)
 
 def database_query(query):
